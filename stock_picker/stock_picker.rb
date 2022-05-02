cee2a43453ab4ead45ef4db504d2d4f51f1a79b4
Graham <git@grahamirvine.com>
2022-05-02 13:17:36 -0600

    Fixed puts line to display correctly by using \ where line breaks in code. Added comments to code

--- stock_picker/stock_picker.rb
+++ stock_picker/stock_picker.rb
@@ -1,18 +1,20 @@
-#require "pry-byebug"
 
 def stock_picker(prices)
   i = 0
+  #price_index holds the lowest price prices index location at 0 and highest index at location 1
   price_index = []
   min_price_index = prices.index(prices.min)
   price_index.push(min_price_index)
   price_difference = prices.drop(min_price_index).max - prices[min_price_index]
   price_index.push(prices.index(prices.drop(min_price_index).max))
+  #a new index that is a copy of prices is created so it can be modified in the loop
   price_array = Array.new(prices)
   while i < (price_array.length - 1) do
     price_array.delete(price_array.min)
+    #min price is deleted and checked again. This is to determine if there is a better day to buy
     min_price_index = price_array.index(price_array.min)
+    # needed to compare prices vs last price to see if new buy day is better profit
     price_difference_looped = price_array.drop(min_price_index).max - price_array.min
-    #binding.pry
     if (price_difference_looped > price_difference)
       price_index = []
       #need to pick the last example of the numbers not the first
@@ -24,8 +26,8 @@ def stock_picker(prices)
       price_difference = price_difference_looped
     i += 1
   end
-  puts price_index
-    print " The best day to buy is Day #{price_index[0]} at $#{prices[price_index[0]]}.\n The best day to sell is Day #{price_index[1]} at $#{prices[price_index[1]]}.\n For a total profit of $#{price_difference_looped}.\n"
+    puts " The best day to buy is Day #{price_index[0]} at $#{prices[price_index[0]]}.\n The best day to sell is Day "\
+     "#{price_index[1]} at $#{prices[price_index[1]]}.\n For a total profit of $#{price_difference_looped}.\n\n"
 end
 
 stock_picker([17,3,6,9,15,8,6,1,10])
