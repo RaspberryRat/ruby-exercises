c913a7c71a6ea96d735280809d77a83d535099d5
G <mail@grahamirvine.com>
2022-07-13 17:08:22 -0600

    refactored bubble sort

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -1,35 +1,25 @@
-def bubble_sort(num_array)
-i = 0
-j = 1
-flag = 0 #loop stops when this is set to 1
+def bubble_sort(array)
+sorted = false
 
-  until flag == 1 do
-    #this comparison is needed to check if the num_array is the same after a loop
-    comparison_array = Array.new(num_array)
-    while j < num_array.length
-      if num_array[i] > num_array[j]
-        num_array.insert(i,num_array.delete_at(j))
+  while !sorted
+    sorted = true
+    i = 0
+    while i < array.length - 1
+      if array[i] > array [i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        sorted = false
       end
       i += 1
-      j += 1
-      end
-    if j == num_array.length
-      if num_array == comparison_array
-        flag = 1 #if the num_array is unchanged after a loop, sort is complete
-        break
-      else
-        i = 0 #reset to start loop over
-        j = 1
-        next
-      end
     end
   end
-  print "This is your sorted array #{num_array}.\n"
+array
 end
 
 
+puts "Sort #1"
 array = [4,3,78,2,0,2]
-bubble_sort(array)
+puts bubble_sort(array)
 
+puts "\n\nSort #2"
 array = [1039103, 12930, 1200, 123, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
-bubble_sort(array)
+puts bubble_sort(array)
